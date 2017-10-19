0a0a21e16a718b6651171492dc309ce55725a3e2
Alert_Qian <1679026896@qq.com>
2017-10-19 17:26:40 +0800

    add jxw

---
+++ application/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'type'            => 'mysql',
+    'hostname'        => '127.0.0.1',
+    'database'        => '0794jxw',
+    'username'        => 'root',
+    'password'        => '',
+    'hostport'        => '3306',
+    'prefix'          => 'tpt_', 
+];
