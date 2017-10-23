7f829b93f2ab8dc85a88c462d540c1b2ecdf7bc6
Alert_Qian <1679026896@qq.com>
2017-10-23 17:29:20 +0800

    complete add jiaxiao

--- application/admin/controller/School.php
+++ application/admin/controller/School.php
@@ -6,25 +6,23 @@ use app\admin\model\School as SchoolModel;
 class School extends Common
 {
     public function index(){
-    	$school=new School();
+    	$school=new SchoolModel();
     	return tptc();
     }
 
     //添加驾校
     public function add(){
-    	$school=new School();
-    	/*if (request()->isPost()) {
+    	$school=new SchoolModel();
+    	if (request()->isPost()) {
             $data = input('post.');
             $data['addtime'] = time();
-            $data['view'] = 1;
-            $data['uid'] = 1;
             $data['content'] = remove_xss(input('content'));
-            if ($content->add($data)) {
+            if ($school->add($data)) {
                 return tpta('添加成功');
             } else {
                 return tptb('添加失败');
             }
-        }*/
+        }
     	return tptc();
     }
 }
